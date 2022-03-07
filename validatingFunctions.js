0b8a9c1ff54000df032090def6a9291fb05b0872
Boffinbrains <88310402+boffinbrains@users.noreply.github.com>
2022-03-07 10:45:17 +0530

    Fetch API

--- validatingFunctions.js
+++ validatingFunctions.js
@@ -1,3 +1,5 @@
+let validationErrors = [];
+
 const tooltip = (message, height = 24) => `
     <div class="validator-tooltip" style="top: ${height + 12}px">
         <img src="./assets/warning.svg" alt="warning" width="20" style="margin-right: 10px"/>
@@ -8,61 +10,152 @@ const tooltip = (message, height = 24) => `
     </div>
 `;
 
+const snackbar = (message, bgColor = "#111", color = "#fff") => `
+    <div class="validator-snackbar" style="background-color: ${bgColor}; color: ${color}">
+        ${message}
+        <button type="button" style="display: contents; cursor: pointer;" onclick="this.parentNode.remove()">
+            <img src="./assets/delete-white.svg" alt="delete" width="20" />
+        </button>
+    </div>
+`;
+
+const removeElement = (selector) => {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(elements => {
+        elements.remove();
+    });
+}
+
 const checkRequired = (_type, value, element) => {
-    if (value != '') {
-        return true;
+    if (element.type === 'radio' || element.type === 'checkbox') {
+        let newElement = document.getElementsByName(element.name);
+        let isChecked = [];
+        for (let i = 0; i < newElement.length; i++) {
+            isChecked.push(newElement[i].checked);
+        }
+        if (isChecked.includes(true)) {
+            validationErrors = validationErrors.filter(error => error != element.name);
+        } else {
+            validationErrors.push(element.name);
+            element.insertAdjacentHTML('afterend', tooltip("Required!", element.offsetHeight));
+        }
     } else {
-        element.insertAdjacentHTML('afterend', tooltip("Required!", element.offsetHeight));
+        if (value != '') {
+            validationErrors = validationErrors.filter(error => error != element.name);
+        } else {
+            validationErrors.push(element.name);
+            element.insertAdjacentHTML('afterend', tooltip("Required!", element.offsetHeight));
+        }
     }
 }
 
 const checkEmail = (_emailOnly, value, element) => {
     let regex = /^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
     if (regex.test(value)) {
-        return true;
+        validationErrors = validationErrors.filter(error => error != element.name);
     } else {
+        validationErrors.push(element.name);
         element.insertAdjacentHTML('afterend', tooltip(`Please include '@' in the email address.`, element.offsetHeight));
     }
 }
 
-const checkMaxlen = (max, value, element) => {
-    if (max >= value.length) {
-        return true;
+const checkMaxlen = (maxlength, value, element) => {
+    if (maxlength >= value.length) {
+        validationErrors = validationErrors.filter(error => error != element.name);
     } else {
-        element.insertAdjacentHTML('afterend', tooltip(`Maximum length should be ${max}.`, element.offsetHeight));
+        validationErrors.push(element.name);
+        element.insertAdjacentHTML('afterend', tooltip(`Maximum length should be ${maxlength}.`, element.offsetHeight));
     }
 }
 
-const checkMinlen = (min, value, element) => {
-    if (min <= value.length) {
-        return true;
+const checkMinlen = (minlength, value, element) => {
+    if (minlength <= value.length) {
+        validationErrors = validationErrors.filter(error => error != element.name);
     } else {
-        element.insertAdjacentHTML('afterend', tooltip(`Minimum length should be ${min}.`, element.offsetHeight));
+        validationErrors.push(element.name);
+        element.insertAdjacentHTML('afterend', tooltip(`Minimum length should be ${minlength}.`, element.offsetHeight));
+    }
+}
+
+const checkMax = (max, _value, element) => {
+    let newElement = document.getElementsByName(element.name);
+    let isChecked = 0;
+    for (let i = 0; i < newElement.length; i++) {
+        if (newElement[i].checked) {
+            ++isChecked;
+        }
+    }
+    if (max >= isChecked) {
+        validationErrors = validationErrors.filter(error => error != element.name);
+    } else {
+        validationErrors.push(element.name);
+        element.insertAdjacentHTML('afterend', tooltip(`Maximum should be ${max}.`, element.offsetHeight));
+    }
+}
+
+const checkMin = (min, _value, element) => {
+    let newElement = document.getElementsByName(element.name);
+    let isChecked = 0;
+    for (let i = 0; i < newElement.length; i++) {
+        if (newElement[i].checked) {
+            ++isChecked;
+        }
+    }
+    if (min <= isChecked) {
+        validationErrors = validationErrors.filter(error => error != element.name);
+    } else {
+        validationErrors.push(element.name);
+        element.insertAdjacentHTML('afterend', tooltip(`Minimum should be ${min}.`, element.offsetHeight));
+    }
+}
+
+const checkFrom = (from, value, element) => {
+    let dateFrom = new Date(from);
+    let date = new Date(value);
+    if (dateFrom.getTime() <= date.getTime()) {
+        validationErrors = validationErrors.filter(error => error != element.name);
+    } else {
+        validationErrors.push(element.name);
+        element.insertAdjacentHTML('afterend', tooltip(`Date before ${from} is invalid.`, element.offsetHeight));
+    }
+}
+
+const checkTo = (to, value, element) => {
+    let dateTo = new Date(to);
+    let date = new Date(value);
+    if (dateTo.getTime() >= date.getTime()) {
+        validationErrors = validationErrors.filter(error => error != element.name);
+    } else {
+        validationErrors.push(element.name);
+        element.insertAdjacentHTML('afterend', tooltip(`Date after ${to} is invalid.`, element.offsetHeight));
     }
 }
 
 const checkNumericOnly = (_numericOnly, value, element) => {
-    let regex = /^[0-9]+$/;
+    let regex = /^(?!0\d)\d*(\.\d+)?$/;
     if (regex.test(value)) {
-        return true;
+        validationErrors = validationErrors.filter(error => error != element.name);
     } else {
+        validationErrors.push(element.name);
         element.insertAdjacentHTML('afterend', tooltip(`Please include only numbers.`, element.offsetHeight));
     }
 }
 
 const checkSize = (size, value, element) => {
     if (size == value.length) {
-        return true;
+        validationErrors = validationErrors.filter(error => error != element.name);
     } else {
+        validationErrors.push(element.name);
         element.insertAdjacentHTML('afterend', tooltip(`Size should be ${size}.`, element.offsetHeight));
     }
 }
 
 const checkPattern = (pattern, value, element) => {
     let regex = pattern;
-    if (!regex.test(value)) {
-        return true;
+    if (regex.test(value)) {
+        validationErrors = validationErrors.filter(error => error != element.name);
     } else {
+        validationErrors.push(element.name);
         element.insertAdjacentHTML('afterend', tooltip(`Please follow the pattern.`, element.offsetHeight));
     }
 }
@@ -70,14 +163,15 @@ const checkPattern = (pattern, value, element) => {
 const checkSpecialCharacters = (_specialCharacters, value, element) => {
     let regex = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
     if (!regex.test(value)) {
-        return true;
+        validationErrors = validationErrors.filter(error => error != element.name);
     } else {
+        validationErrors.push(element.name);
         element.insertAdjacentHTML('afterend', tooltip(`Special Characters are not allowed!`, element.offsetHeight));
     }
 }
 
 // Master
-const validationCheck = (value, { max, type, min, size, emailOnly, numericOnly, pattern, specialCharacters }, element) => {
+const validationCheck = (value, { min, max, from, to, maxlength, type, minlength, size, emailOnly, numericOnly, pattern, specialCharacters }, element) => {
     if (type != undefined && type === "required") {
         checkRequired(type, value, element);
     }
@@ -87,11 +181,23 @@ const validationCheck = (value, { max, type, min, size, emailOnly, numericOnly, 
     if (numericOnly != undefined && numericOnly) {
         checkNumericOnly(numericOnly, value, element);
     }
+    if (maxlength != undefined) {
+        checkMaxlen(maxlength, value, element);
+    }
+    if (minlength != undefined) {
+        checkMinlen(minlength, value, element);
+    }
     if (max != undefined) {
-        checkMaxlen(max, value, element);
+        checkMax(max, value, element);
     }
     if (min != undefined) {
-        checkMinlen(min, value, element);
+        checkMin(min, value, element);
+    }
+    if (from != undefined) {
+        checkFrom(from, value, element);
+    }
+    if (to != undefined) {
+        checkTo(to, value, element);
     }
     if (size != undefined) {
         checkSize(size, value, element);
@@ -106,13 +212,20 @@ const validationCheck = (value, { max, type, min, size, emailOnly, numericOnly, 
 
 export {
     tooltip,
+    snackbar,
+    removeElement,
     checkEmail,
     checkMaxlen,
     checkMinlen,
+    checkMax,
+    checkMin,
+    checkFrom,
+    checkTo,
     checkNumericOnly,
     checkSize,
     checkPattern,
     checkRequired,
     checkSpecialCharacters,
-    validationCheck
+    validationCheck,
+    validationErrors
 };
